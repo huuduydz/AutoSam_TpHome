1c6402113d4708af2e37442da11eddcb75e1b7ce
huuduydz <165132829+huuduydz@users.noreply.github.com>
2026-05-11 00:40:39 +0700

    v43-fix storgae

--- kinghop.lua
+++ kinghop.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/af11746e94ff5f48d641a4343d285cbc.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/e2f1c6e8c37c4649942e17331baf3dd4_obf.lua",true))()
