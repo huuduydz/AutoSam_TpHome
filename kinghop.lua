8ffd3909f21f33c74fe84a34a3c6e4f4fda865e2
huuduydz <165132829+huuduydz@users.noreply.github.com>
2026-05-14 03:09:29 +0700

    Update kinghop.lua

--- kinghop.lua
+++ kinghop.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/e2f1c6e8c37c4649942e17331baf3dd4_obf.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/0f3662d31f2747fc861b9f89f43796bc_obf.lua",true))()
