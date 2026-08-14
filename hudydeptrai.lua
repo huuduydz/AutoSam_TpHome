ddc3e233d36313e6721a775b0717ffb9f6cdf173
huuduydz <165132829+huuduydz@users.noreply.github.com>
2026-08-15 05:42:21 +0700

    Update hudydeptrai.lua

--- hudydeptrai.lua
+++ hudydeptrai.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/fdac4fc5eb164ec595851e36c1055321_obf.lua",true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/b550cc19bbde48029c41b96d5d173891_obf.lua",true))()
